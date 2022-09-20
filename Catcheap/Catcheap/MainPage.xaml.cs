c8d8e49778aac3ee6a1a31e20fc3847cf99527f1
Andrius Šukys <93678816+Andrius-Sukys@users.noreply.github.com>
2022-09-20 20:51:28 +0300

    Revert "Add car window"

--- Catcheap/Catcheap/MainPage.xaml.cs
+++ Catcheap/Catcheap/MainPage.xaml.cs
@@ -2,15 +2,23 @@
 
 public partial class MainPage : ContentPage
 {
+	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-    private async void AddCarClicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("//AddCar");
-    }
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
 }
 
