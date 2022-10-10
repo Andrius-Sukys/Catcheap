b808614195ef2513013d490b8174eb7dd02f45ba
ignasvoveriukas9 <91431454+ignasvoveriukas9@users.noreply.github.com>
2022-10-10 12:05:11 +0300

    Enum

--- Catcheap/Catcheap/CarLoaderSaver.cs
+++ Catcheap/Catcheap/CarLoaderSaver.cs
@@ -8,42 +8,55 @@ namespace Catcheap
     {
         private FileIO file = new FileIO();
 
-        private string ManufacturerPattern = "Manufacturer: ";
-        private string ModelPattern = "Model: ";
-        private string MileagePattern = "Mileage: ";
-        private string ExpectedRangePattern = "Expected range: ";
-        private string BatteryCapacityPattern = "Battery size: ";
-        private string ConsumptionPattern = "Consumption rate: ";
-        private string BatteryLevelPattern = "Battery level: ";
+        enum Pattern : int
+        {
+            Manufacturer,
+            Model,
+            Mileage,
+            ExpectedRange,
+            BatteryCapacity,
+            Consumption,
+            BatteryLevel,
+        }
 
         public void Load(Car car, String fileName = "carinfo.txt")
         {
             string temp = file.ReadTextFile(fileName);
 
-            car.Manufacturer = Regex.Replace(Regex.Match(temp, @"\b" + ManufacturerPattern + @"\S*").Value, @"\b" + ManufacturerPattern, "");
+            foreach (Match match in Regex.Matches(temp, @"\b\d \S*"))
+            {
+                int en = Int16.Parse(Regex.Match(match.Value, @"\b\d").Value);
 
-            car.Model = Regex.Replace(Regex.Match(temp, @"\b" + ModelPattern + @"\S*").Value, @"\b" + ModelPattern, "");
-
-            car.Mileage = Double.Parse(Regex.Replace(Regex.Match(temp, @"\b" + MileagePattern + @"\S*").Value, @"\b" + MileagePattern, ""));
-
-            car.ExpectedRange = Math.Round(Double.Parse(Regex.Replace(Regex.Match(temp, @"\b" + ExpectedRangePattern + @"\S*").Value, @"\b" + ExpectedRangePattern, "")), 2);
-
-            car.BatteryCapacity = Double.Parse(Regex.Replace(Regex.Match(temp, @"\b" + BatteryCapacityPattern + @"\S*").Value, @"\b" + BatteryCapacityPattern, ""));
-
-            car.Consumption = Double.Parse(Regex.Replace(Regex.Match(temp, @"\b" + ConsumptionPattern + @"\S*").Value, @"\b" + ConsumptionPattern, ""));
-
-            car.BatteryLevel = Math.Round(Double.Parse(Regex.Replace(Regex.Match(temp, @"\b" + BatteryLevelPattern + @"\S*").Value, @"\b" + BatteryLevelPattern, "")), 2);
+                switch (en)
+                {
+                    case ((int)Pattern.Manufacturer): car.Manufacturer = Regex.Replace(match.Value, @"\b\d ", "");
+                        break;
+                    case ((int)Pattern.Model): car.Model = Regex.Replace(match.Value, @"\b\d ", "");
+                        break;
+                    case ((int)Pattern.Mileage): car.Mileage = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
+                        break;
+                    case ((int)Pattern.ExpectedRange): car.ExpectedRange = Math.Round(Double.Parse(Regex.Replace(match.Value, @"\b\d ", "")), 2);
+                        break;
+                    case ((int)Pattern.BatteryCapacity): car.BatteryCapacity = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
+                        break;
+                    case ((int)Pattern.Consumption): car.Consumption = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
+                        break;
+                    case ((int)Pattern.BatteryLevel): car.BatteryLevel = Math.Round(Double.Parse(Regex.Replace(match.Value, @"\b\d ", "")), 2);
+                        break;
+                    default: break;
+                }
+            }
         }
 
         public void Save(Car car, String fileName = "carinfo.txt")
         {
-            file.WriteTextToFile(ManufacturerPattern + car.Manufacturer + '\n' +
-                                 ModelPattern + car.Model + '\n' +
-                                 MileagePattern + car.Mileage + '\n' +
-                                 ExpectedRangePattern + car.ExpectedRange + '\n' +
-                                 BatteryCapacityPattern + car.BatteryCapacity + '\n' +
-                                 ConsumptionPattern + car.Consumption + '\n' +
-                                 BatteryLevelPattern + car.BatteryLevel + '\n', "carinfo.txt");
+            file.WriteTextToFile(((int)Pattern.Manufacturer) + " " + car.Manufacturer + '\n' +
+                                 ((int)Pattern.Model) + " " + car.Model + '\n' +
+                                 ((int)Pattern.Mileage) + " " + car.Mileage + '\n' +
+                                 ((int)Pattern.ExpectedRange) + " " + car.ExpectedRange + '\n' +
+                                 ((int)Pattern.BatteryCapacity) + " " + car.BatteryCapacity + '\n' +
+                                 ((int)Pattern.Consumption) + " " + car.Consumption + '\n' +
+                                 ((int)Pattern.BatteryLevel) + " " + car.BatteryLevel + '\n', "carinfo.txt");
         }
 
     }
