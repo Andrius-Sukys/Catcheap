67dc74b08aeaf3e3e031a2f1bcffb30d1345bf38
ignasvoveriukas9 <91431454+ignasvoveriukas9@users.noreply.github.com>
2022-10-11 22:29:09 +0300

    fix LoeaderSaver

--- Catcheap/Catcheap/CarLoaderSaver.cs
+++ Catcheap/Catcheap/CarLoaderSaver.cs
@@ -23,27 +23,38 @@ namespace Catcheap
         {
             string temp = file.ReadTextFile(fileName);
 
-            foreach (Match match in Regex.Matches(temp, @"\b\d \S*"))
-            {
-                int en = Int16.Parse(Regex.Match(match.Value, @"\b\d").Value);
 
-                switch (en)
+            if (temp != null)
+            {
+                foreach (Match match in Regex.Matches(temp, @"\b\d \S*"))
                 {
-                    case ((int)Pattern.Manufacturer): car.Manufacturer = Regex.Replace(match.Value, @"\b\d ", "");
-                        break;
-                    case ((int)Pattern.Model): car.Model = Regex.Replace(match.Value, @"\b\d ", "");
-                        break;
-                    case ((int)Pattern.Mileage): car.Mileage = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
-                        break;
-                    case ((int)Pattern.ExpectedRange): car.ExpectedRange = Math.Round(Double.Parse(Regex.Replace(match.Value, @"\b\d ", "")), 2);
-                        break;
-                    case ((int)Pattern.BatteryCapacity): car.BatteryCapacity = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
-                        break;
-                    case ((int)Pattern.Consumption): car.Consumption = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
-                        break;
-                    case ((int)Pattern.BatteryLevel): car.BatteryLevel = Math.Round(Double.Parse(Regex.Replace(match.Value, @"\b\d ", "")), 2);
-                        break;
-                    default: break;
+                    int en = Int16.Parse(Regex.Match(match.Value, @"\b\d").Value);
+
+                    switch (en)
+                    {
+                        case ((int)Pattern.Manufacturer):
+                            car.Manufacturer = Regex.Replace(match.Value, @"\b\d ", "");
+                            break;
+                        case ((int)Pattern.Model):
+                            car.Model = Regex.Replace(match.Value, @"\b\d ", "");
+                            break;
+                        case ((int)Pattern.Mileage):
+                            car.Mileage = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
+                            break;
+                        case ((int)Pattern.ExpectedRange):
+                            car.ExpectedRange = Math.Round(Double.Parse(Regex.Replace(match.Value, @"\b\d ", "")), 2);
+                            break;
+                        case ((int)Pattern.BatteryCapacity):
+                            car.BatteryCapacity = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
+                            break;
+                        case ((int)Pattern.Consumption):
+                            car.Consumption = Double.Parse(Regex.Replace(match.Value, @"\b\d ", ""));
+                            break;
+                        case ((int)Pattern.BatteryLevel):
+                            car.BatteryLevel = Math.Round(Double.Parse(Regex.Replace(match.Value, @"\b\d ", "")), 2);
+                            break;
+                        default: break;
+                    }
                 }
             }
 
--- Catcheap/Catcheap/JourneysLoaderSaver.cs
+++ Catcheap/Catcheap/JourneysLoaderSaver.cs
@@ -13,17 +13,20 @@ namespace Catcheap
 
             string temp = file.ReadTextFile(fileName);
 
-            foreach (Match match in Regex.Matches(temp, @"\bDistance: \S* Date: \S*"))
+            if (temp != null)
             {
-                Journey newDistance = new Journey();
-                string tempDate = Regex.Replace(Regex.Match(match.Value, @"\bDate: \S*").Value, @"\bDate: ", "");
-                string tempDist = Regex.Replace(Regex.Match(match.Value, @"\bDistance: \S*").Value, @"\bDistance: ", "");
+                foreach (Match match in Regex.Matches(temp, @"\bDistance: \S* Date: \S*"))
+                {
+                    Journey newDistance = new Journey();
+                    string tempDate = Regex.Replace(Regex.Match(match.Value, @"\bDate: \S*").Value, @"\bDate: ", "");
+                    string tempDist = Regex.Replace(Regex.Match(match.Value, @"\bDistance: \S*").Value, @"\bDistance: ", "");
 
-                newDistance.Date = DateTime.Parse(tempDate);
-                newDistance.Dist = Int16.Parse(tempDist);
+                    newDistance.Date = DateTime.Parse(tempDate);
+                    newDistance.Dist = Int16.Parse(tempDist);
 
-                journeys.AddJourney(newDistance);
+                    journeys.AddJourney(newDistance);
 
+                }
             }
         }
 
