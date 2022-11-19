2e30a3ac081ff29eca027df51c7d124f5904dbee
ignasvoveriukas9 <91431454+ignasvoveriukas9@users.noreply.github.com>
2022-11-19 11:57:06 +0200

    Update MauiProgram.cs

--- Catcheap/Catcheap/MauiProgram.cs
+++ Catcheap/Catcheap/MauiProgram.cs
@@ -23,7 +23,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            //.UseLocalNotification()
+            .UseLocalNotification()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
