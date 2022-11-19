ba558544bde04151144b0d325dfd97d237579335
ignasvoveriukas9 <91431454+ignasvoveriukas9@users.noreply.github.com>
2022-11-19 16:36:54 +0200

    Test for ValidateInput

---
+++ Catcheap/TestProject/ValidateInputTest.cs
@@ -0,0 +1,32 @@
+﻿using Catcheap.Models.Validation_Classes;
+
+namespace TestProject
+{
+    public class ValidateInputTest
+    {
+
+        ValidateInput validate = new ValidateInput();
+
+        [Theory]
+        [InlineData("10", true)]
+        [InlineData("-1", false)]
+        [InlineData("sada", false)]
+        public void ValidateInputAsAPositiveNumberTest(string par, bool expectedResult)
+        {
+
+            Assert.True(validate.ValidateInputAsAPositiveNumber(par) == expectedResult);
+
+        }
+
+        [Theory]
+        [InlineData("", true)]
+        [InlineData(null, true)]
+        [InlineData("sada", false)]
+        public void ValidateInputAsNull(string par, bool expectedResult)
+        {
+
+            Assert.True(validate.ValidateInputAsNull(par) == expectedResult);
+
+        }
+    }
+}
