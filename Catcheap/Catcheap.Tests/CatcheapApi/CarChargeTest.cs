14894c6a2b11cc498cfee8f05db0043045c74975
ignasvoveriukas9 <91431454+ignasvoveriukas9@users.noreply.github.com>
2022-12-20 12:08:26 +0200

    Tests for api models

---
+++ Catcheap/Catcheap.Tests/CatcheapApi/CarChargeTest.cs
@@ -0,0 +1,20 @@
+﻿using Catcheap.API.Models.CarModels;
+
+
+namespace Catcheap.Tests.CatcheapApi
+{
+    public class CarChargeTest
+    {
+        [Fact]
+        public void CompareToTest()
+        {
+            CarCharge charge1 = new CarCharge();
+            CarCharge charge2 = new CarCharge();
+
+            charge1.StartOfCharge = new DateTime(2022,12,13,12,0,0);
+            charge2.StartOfCharge = new DateTime(2022, 12, 12, 12, 0, 0);
+
+            Assert.Equal(1, charge1.CompareTo(charge2));
+        }
+    }
+}
